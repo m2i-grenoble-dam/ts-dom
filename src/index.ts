2aa71aa0ee8027dd81ada44767306ec7c37ee28a
Jean Demel <jean@demel.com>
2023-05-24 11:53:19 +0200

    dom exercise

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+let para2 = document.querySelector<HTMLElement>('#para2');
+para2.style.color = 'blue';
+
+let section2 = document.querySelector<HTMLElement>('#section2');
+section2.style.borderStyle = "dotted";
+
+let colorful = document.querySelector<HTMLElement>('#section2 .colorful');
+colorful.style.background = 'orange'
+
+
+let h2Sect1 = document.querySelector<HTMLElement>('#section1 h2');
+h2Sect1.style.fontStyle = 'italic';
+
+let colorfulInPara = document.querySelector<HTMLElement>('p .colorful');
+colorfulInPara.style.display = 'none';
+
+para2.textContent = 'Modified by TS'
+
+let a = document.querySelector<HTMLAnchorElement>('a');
+a.href = 'http://www.m2iformation.fr';
+
+let h2Sect2 = document.querySelector<HTMLElement>('#section2 h2');
+h2Sect2.classList.add('big-text');
+
+let paras = document.querySelectorAll('p');
+for (const item of paras) {
+    item.style.fontStyle = 'italic';
+}

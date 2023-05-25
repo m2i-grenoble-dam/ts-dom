24d8fef9ec154200b22b5c7fb5c34b62c1bf315c
Jean D <demeljean@hotmail.fr>
2023-05-25 13:56:21 +0200

    list mvc

--- src/exo-list.ts
+++ src/exo-list.ts
@@ -1,5 +1,6 @@
 const btnAdd = document.querySelector<HTMLElement>('button');
 const ul = document.querySelector<HTMLElement>('ul');
+const input = document.querySelector<HTMLInputElement>('input');
 
 let list:string[] = ['Valeur 1', 'Valeur 2', 'Valeur 3'];
 
@@ -7,18 +8,24 @@ displayList(list);
 
 
 
+
+btnAdd.addEventListener('click', () => {
+    list.push(input.value);
+    displayList(list);
+});
+
 function displayList(list:string[]) {
+    ul.innerHTML = '';
     for (const item of list) {
         const li = document.createElement('li');
-        li.textContent = item;
+        li.textContent = item
         ul.appendChild(li);
     }
 }
 
 
 
-btnAdd.addEventListener('click', () => {
-    const li = document.createElement('li');
-    li.textContent = 'texte';
-    ul.appendChild(li);
+document.querySelector<HTMLElement>('#afficher-tableau').addEventListener('click', () => {
+    console.log(list);
+    
 });
